766bc0c89009939df8dec31da111fd5edc41e5b8
Richard Roberts <richard.andrew.roberts@gmail.com>
2019-10-22 01:11:41 +1300

    Simplify SelectCommand

--- src/SelectCommand.cpp
+++ src/SelectCommand.cpp
@@ -1,10 +1,3 @@
-//
-//  SelectCommand.cpp
-//  SalientPosesMaya
-//
-//  Created by Richard Roberts on 12/09/18.
-//
-
 #include <iomanip>
 #include <sstream>
 #include <vector>
@@ -24,61 +17,63 @@
 #include <maya/MFnTypedAttribute.h>
 #include <maya/MAngle.h>
 
-#include "../SalientPosesPerformance/src/OpenCLProcess.hpp"
 #include "SelectCommand.hpp"
 #include "MayaUtils.hpp"
 #include "../SalientPosesPerformance/src/ErrorTable.hpp"
-#include "../SalientPosesPerformance/src/SelectionManager.hpp"
+#include "../SalientPosesPerformance/src/Selector.hpp"
 
-#define RAD_TO_DEG 57.2958279088
 
-MString SelectCommand::openCLDirectory;
+#define VERBOSE 2
 
-// Set name and flags
 const char* SelectCommand::kName = "salientSelect";
-const char* SelectCommand::kOpenCLPlatformFlagShort = "-clp";
-const char* SelectCommand::kOpenCLPlatformFlagLong = "-clplatform";
-const char* SelectCommand::kOpenCLDeviceFlagShort = "-cld";
-const char* SelectCommand::kOpenCLDeviceFlagLong = "-cldevice";
-const char* SelectCommand::kStartFlagShort = "-s";
-const char* SelectCommand::kStartFlagLong = "-start";
-const char* SelectCommand::kFinishFlagShort = "-f";
-const char* SelectCommand::kFinishFlagLong = "-finish";
-const char* SelectCommand::kAnimFlagShort = "-a";
-const char* SelectCommand::kAnimFlagLong = "-anim";
-const char* SelectCommand::kDimensionsFlagShort = "-d";
-const char* SelectCommand::kDimensionsFlagLong = "-dimensions";
-const char* SelectCommand::kHelpFlagShort = "-h";
-const char* SelectCommand::kHelpFlagLong = "-help";
-
 
 MStatus SelectCommand::doIt(const MArgList& args) {
     MStatus status;
     
-    // Process arguments
     status = GatherCommandArguments(args);
     CHECK_MSTATUS_AND_RETURN_IT(status);
     
-    // Set OpenCL platform and device
-    OpenCLProcessManager::setPlatformIdToPlatformAtIndex(openCLPlatformIndex - 1);
-    OpenCLProcessManager::setDeviceIdToDeviceAtIndex(openCLDeviceIndex - 1);
-    
     // Ensure the start and end frames are given as argumetns
-    if (_start == -1 || _finish == -1) {
-		std::ostringstream os;
-		os << "The start and end flags have not been set (start=" << _start << " end=" << _finish << ")";
+    if (start == -1 || end == -1) {
+        std::ostringstream os;
+        os << "The start and end flags have not been set (start=" << start << " end=" << end << ")";
         Log::error(os.str());
         return MS::kFailure;
     }
     
+    if (VERBOSE == 1) {
+        std::ostringstream os;
+        os << "Start: " << start << std::endl;
+        os << "End: " << end << std::endl;
+        os << "Data: " << std::endl;
+        os << animData << std::endl;
+        MGlobal::displayInfo(os.str().c_str());
+    }
+    
     // Perform the selection
-    std::string openclProgramPath = openCLDirectory.asChar() + std::string("/kernel.cl");
-    std::string openclProgramName = "max_distance_to_polyline";
-	AnimationProxy anim = AnimationProxy::fromData(_animData, _dimensions, _start, _finish);
-    SelectionManager manager = SelectionManager(anim, _fixedKeyframes.data(), _fixedKeyframes.size(),
-                                                openclProgramPath, openclProgramName);
-    manager.incrementUntilNKeyframes(anim.nFrames);
-    SelectionProxy * selectionProxy = manager.asProxy();
+    int maxKeyframes = end - start + 1;
+    AnimationProxy anim = AnimationProxy(animData);
+    ErrorTable table = ErrorTable(anim);
+    
+    if (VERBOSE == 2) {
+        anim.save("/Users/richard-roberts/Desktop/anim.csv");
+        table.save("/Users/richard-roberts/Desktop/table.csv");
+    }
+    
+    SelectionProxy selections = Select::upToN(end - start + 1, maxKeyframes, anim, table);
+    
+    if (VERBOSE == 3) {
+        std::ostringstream os;
+        for (int i = 3; i < maxKeyframes + 1; i++) {
+            std::vector<int> sel = selections.getSelectionByNKeyframes(i);
+            os << i << ": ";
+            for (int j = 0; j < sel.size(); j++) {
+                os << sel[j] << ",";
+            }
+            os << std::endl;
+        }
+        MGlobal::displayInfo(os.str().c_str());
+    }
 
     // Build string containing result (precise to four decimal places)
     std::ostringstream ret;
@@ -88,71 +83,35 @@ MStatus SelectCommand::doIt(const MArgList& args) {
     //   e|a,b,c
     //     where e is error, | is a delimiter, and a,b,c are the selection (wthout spaces).
     // Each error-selection pair is delimited by a new line.
-    for (int i = 0; i < selectionProxy->numberOfSelections(); i++) {
-        std::vector<int> selection = selectionProxy->getSelectionByIndex(i);
-        ret << selectionProxy->getErrorByIndex(i) << "|";
+    for (int i = 3; i < maxKeyframes + 1; i++) {
+        float error = selections.getErrorByNKeyframes(i);
+        std::vector<int> selection = selections.getSelectionByNKeyframes(i);
+        ret << error << "|";
         ret << selection[0];
-        for (int j = 1; j < selection.size(); j++) {
-            ret << "," << selection[j];
-        }
+        for (int j = 1; j < selection.size(); j++) ret << "," << selection[j];
         ret << "\n";
     }
 
 	setResult(MString(ret.str().c_str()));
-
-	delete selectionProxy;
     return MS::kSuccess;
 }
 
-MSyntax SelectCommand::newSyntax() {
-    MSyntax syntax;
-    syntax.addFlag(kHelpFlagShort, kHelpFlagLong);
-    syntax.addFlag(kOpenCLPlatformFlagShort, kOpenCLPlatformFlagLong, MSyntax::kLong);
-    syntax.addFlag(kOpenCLDeviceFlagShort, kOpenCLDeviceFlagLong, MSyntax::kLong);
-    syntax.addFlag(kStartFlagShort, kStartFlagLong, MSyntax::kLong);
-    syntax.addFlag(kFinishFlagShort, kFinishFlagLong, MSyntax::kLong);
-	syntax.addFlag(kAnimFlagShort, kAnimFlagLong, MSyntax::kDouble);
-	syntax.makeFlagMultiUse(kAnimFlagShort);
-	syntax.addFlag(kDimensionsFlagShort, kDimensionsFlagLong, MSyntax::kLong);
-	syntax.makeFlagMultiUse(kDimensionsFlagShort);
-    syntax.addFlag(kDimensionsFlagShort, kDimensionsFlagLong, MSyntax::kLong);
-    syntax.setObjectType(MSyntax::kSelectionList, 0, 255);
-    syntax.useSelectionAsDefault(false);
-    return syntax;
-}
-
 MStatus SelectCommand::GatherCommandArguments(const MArgList& args) {
-	MStatus status;
-
-	unsigned int ix = 0;
+    unsigned int ix = 0;
+	start = args.asInt(ix++);
+	end = args.asInt(ix++);
     
-    openCLPlatformIndex = args.asInt(ix, &status);
-    ix += 1;
-    openCLDeviceIndex = args.asInt(ix, &status);
-    ix += 1;
-
-	_start = args.asInt(ix,&status);
-	ix += 1;
-	_finish = args.asInt(ix, &status);
-	ix += 1;
-	MDoubleArray mAnimData = args.asDoubleArray(ix, &status);
-	ix += 1;
-	MStringArray mDimensions = args.asStringArray(ix, &status);
-    ix += 1;
-    MIntArray mFixedKeyframes = args.asIntArray(ix, &status);
-    ix += 1;
-    
+    MDoubleArray mAnimData = args.asDoubleArray(ix);
+    int nFrames = end - start + 1;
+    int nDims = mAnimData.length() / nFrames;
+    animData = Eigen::MatrixXf(nDims, nFrames);
+    int f = 0;
+    int d = 0;
 	for (uint i = 0; i < mAnimData.length(); i++) {
-		_animData.push_back(mAnimData[i]);
+		animData(d, f) = mAnimData[i];
+        d += 1;
+        if (d == nDims) { d = 0; f += 1; }
 	}
-
-	for (uint i = 0; i < mDimensions.length(); i++) {
-		_dimensions.push_back(mDimensions[i].asChar());
-	}
-    
-    for (uint i = 0; i < mFixedKeyframes.length(); i++) {
-        _fixedKeyframes.push_back(mFixedKeyframes[i]);
-    }
     
     return MS::kSuccess;
 }
--- src/SelectCommand.hpp
+++ src/SelectCommand.hpp
@@ -1,14 +1,8 @@
-//
-//  SelectCommand.hpp
-//  SalientPosesMaya
-//
-//  Created by Richard Roberts on 12/09/18.
-//
-
-#ifndef SelectCommand_hpp
-#define SelectCommand_hpp
+#pragma once
 
 #include <vector>
+
+#include "../eigen-git-mirror/Eigen/Dense"
 
 #include <maya/MArgList.h>
 #include <maya/MSyntax.h>
@@ -24,37 +18,13 @@ public:
     virtual MStatus  redoIt() { return MS::kSuccess; }
     virtual bool isUndoable() const { return false; }
     static void* creator() { return new SelectCommand; }
-    static MSyntax newSyntax();
-    
     const static char* kName;
-    const static char* kOpenCLPlatformFlagShort;
-    const static char* kOpenCLPlatformFlagLong;
-    const static char* kOpenCLDeviceFlagShort;
-    const static char* kOpenCLDeviceFlagLong;
-    const static char* kHelpFlagShort;
-    const static char* kHelpFlagLong;
-    const static char* kStartFlagShort;
-    const static char* kStartFlagLong;
-    const static char* kFinishFlagShort;
-    const static char* kFinishFlagLong;
-	const static char* kAnimFlagShort;
-	const static char* kAnimFlagLong;
-	const static char* kDimensionsFlagShort;
-	const static char* kDimensionsFlagLong;
-
-	static MString openCLDirectory;
     
 private:
-    
-    int _start = -1;
-    int _finish = -1;
-	std::vector<float> _animData;
-    std::vector<int> _fixedKeyframes;
-	std::vector<std::string> _dimensions;
-    int openCLPlatformIndex;
-    int openCLDeviceIndex;
     MStatus GatherCommandArguments(const MArgList& args);
+    
+    int start;
+    int end;
+    Eigen::MatrixXf animData;
+    
 };
-
-
-#endif /* SelectCommand_hpp */
