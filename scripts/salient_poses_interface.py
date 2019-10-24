a5011eca71a02744a3672693a4edd36f42286c3b
Richard Roberts <rykardo.r@gmail.com>
2019-10-24 18:19:36 +1300

    Update the interface

--- scripts/salient_poses_interface.py
+++ scripts/salient_poses_interface.py
@@ -21,81 +21,89 @@ class SalientPosesGUI(StandardMayaWindow):
     def __init__(self):
         super(SalientPosesGUI, self).__init__("Salient Poses")
         
-        self.selections = {}
-        self.select_attr_gui = AttributeSelectorForCurrentSelection("Attributes for Selection", [])
-        self.reduce_attr_gui = AttributeSelectorForCurrentSelection("Attributes for Reduction", [])
+        self.extreme_selections = {}
+        self.breakdown_selections = {}
+        self.current_selection = []
+        self.select_attr_gui = AttributeSelectorForCurrentSelection("Attributes for Selection", [], parent=self)
+        self.reduce_attr_gui = AttributeSelectorForCurrentSelection("Attributes for Reduction", [], parent=self)
         
         self.create_widgets()
         self.create_layouts()
         self.create_connections()
         
     def create_widgets(self):
-        self.choose_select_attr_button = QtWidgets.QPushButton("Choose")
-        self.choose_reduce_attr_button = QtWidgets.QPushButton("Choose")
+        self.choose_select_attr_button = QtWidgets.QPushButton("Attrs (S)")
+        self.choose_reduce_attr_button = QtWidgets.QPushButton("Attrs (R)")
         
-        self.select_button = QtWidgets.QPushButton("Select")
-        self.reduce_button = QtWidgets.QPushButton("Reduce")
+        self.fixed_keyframes_label = QtWidgets.QLabel("Fixed Keyframes")
+        self.fixed_keyframes_edit = QtWidgets.QLineEdit()
+        
+        self.select_extremes_button = QtWidgets.QPushButton("Extremes")
+        self.n_extreme_keyframes_label = QtWidgets.QLabel("N")
+        self.n_extreme_keyframes_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.n_extreme_keyframes_edit = QtWidgets.QLineEdit(str(self.n_extreme_keyframes_slider.value()))
+        self.n_extreme_keyframes_edit.setEnabled(False)
+        self.extreme_reduce_button = QtWidgets.QPushButton("Reduce by Extremes Only")
+        
+        self.select_breakdowns_button = QtWidgets.QPushButton("Breakdowns")
+        self.n_breakdown_keyframes_label = QtWidgets.QLabel("N")
+        self.n_breakdown_keyframes_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.n_breakdown_keyframes_edit = QtWidgets.QLineEdit(str(self.n_breakdown_keyframes_slider.value()))
+        self.n_breakdown_keyframes_edit.setEnabled(False)
+        self.breakdown_reduce_button = QtWidgets.QPushButton("Reduce by Breakdowns Too")
+        
         self.undo_button = QtWidgets.QPushButton("Undo")
-        
-        self.n_frames_label = QtWidgets.QLabel("Number of Frames")
-        self.n_frames_edit = QtWidgets.QLineEdit(str(self.read_n_frames()))
-        self.n_frames_edit.setEnabled(False)
-        
-        self.max_keyframes_label = QtWidgets.QLabel("Maximum Keyframes")
-        self.max_keyframes_edit = QtWidgets.QLineEdit(str(int(self.read_n_frames() * 0.2)))
-        self.max_keyframes_edit.setEnabled(False)
-        
-        self.n_keyframes_label = QtWidgets.QLabel("N Keyframes")
-        self.n_keyframes_slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
-        self.n_keyframes_edit = QtWidgets.QLineEdit(str(self.n_keyframes_slider.value()))
-        self.n_keyframes_edit.setEnabled(False)
-        
         self.close_button = QtWidgets.QPushButton("Close")
         
     def create_layouts(self):
         button_layout_choose = QtWidgets.QHBoxLayout()
-        button_layout_choose.addWidget(self.choose_select_attr_button)
+        button_layout_choose.addWidget(self.choose_select_attr_button)        
         button_layout_choose.addWidget(self.choose_reduce_attr_button)
+        
+        fixed_keyframes_layout = QtWidgets.QHBoxLayout()
+        fixed_keyframes_layout.addWidget(self.fixed_keyframes_label)
+        fixed_keyframes_layout.addWidget(self.fixed_keyframes_edit)
                 
-        button_layout_select = QtWidgets.QHBoxLayout()
-        button_layout_select.addWidget(self.select_button)        
+        extreme_layout = QtWidgets.QHBoxLayout()
+        extreme_layout.addWidget(self.select_extremes_button)        
+        extreme_layout.addWidget(self.n_extreme_keyframes_label)
+        extreme_layout.addWidget(self.n_extreme_keyframes_slider)
+        extreme_layout.addWidget(self.n_extreme_keyframes_edit)
+        extreme_button_layout = QtWidgets.QHBoxLayout()
+        extreme_button_layout.addWidget(self.extreme_reduce_button)
         
-        keyframes_layout_n_frames = QtWidgets.QHBoxLayout()
-        keyframes_layout_n_frames.addWidget(self.n_frames_label)
-        keyframes_layout_n_frames.addWidget(self.n_frames_edit)
-        
-        keyframes_layout_max_keyframes = QtWidgets.QHBoxLayout()
-        keyframes_layout_max_keyframes.addWidget(self.max_keyframes_label)
-        keyframes_layout_max_keyframes.addWidget(self.max_keyframes_edit)
-        
-        keyframes_layout_n_keyframes = QtWidgets.QHBoxLayout()
-        keyframes_layout_n_keyframes.addWidget(self.n_keyframes_label)
-        keyframes_layout_n_keyframes.addWidget(self.n_keyframes_slider)
-        keyframes_layout_n_keyframes.addWidget(self.n_keyframes_edit)
-        
-        button_layout_reduce = QtWidgets.QHBoxLayout()
-        button_layout_reduce.addWidget(self.reduce_button)
-        button_layout_reduce.addWidget(self.undo_button)
+        breakdown_layout = QtWidgets.QHBoxLayout()
+        breakdown_layout.addWidget(self.select_breakdowns_button)        
+        breakdown_layout.addWidget(self.n_breakdown_keyframes_label)
+        breakdown_layout.addWidget(self.n_breakdown_keyframes_slider)
+        breakdown_layout.addWidget(self.n_breakdown_keyframes_edit)
+        breakdown_button_layout = QtWidgets.QHBoxLayout()
+        breakdown_button_layout.addWidget(self.breakdown_reduce_button)
         
         button_layout_bot = QtWidgets.QHBoxLayout()
+        button_layout_bot.addWidget(self.undo_button)
         button_layout_bot.addWidget(self.close_button)
             
         main_layout = QtWidgets.QVBoxLayout(self)
         main_layout.addLayout(button_layout_choose)
-        main_layout.addLayout(button_layout_select)
-        main_layout.addLayout(keyframes_layout_n_frames)
-        main_layout.addLayout(keyframes_layout_max_keyframes)
-        main_layout.addLayout(keyframes_layout_n_keyframes)
-        main_layout.addLayout(button_layout_reduce)
+        main_layout.addLayout(fixed_keyframes_layout)
+        main_layout.addLayout(extreme_layout)
+        main_layout.addLayout(extreme_button_layout)
+        main_layout.addLayout(breakdown_layout)
+        main_layout.addLayout(breakdown_button_layout)
         main_layout.addLayout(button_layout_bot)
         
     def create_connections(self):
         self.choose_select_attr_button.clicked.connect(self.open_choose_for_selection_dialog)
         self.choose_reduce_attr_button.clicked.connect(self.open_choose_for_reduction_dialog)
-        self.select_button.clicked.connect(self.select)
-        self.reduce_button.clicked.connect(self.reduce)
+        self.select_extremes_button.clicked.connect(self.select_extremes)
+        self.select_breakdowns_button.clicked.connect(self.select_breakdowns)
+        self.extreme_reduce_button.clicked.connect(self.reduce_using_extremes_only)
+        self.breakdown_reduce_button.clicked.connect(self.reduce_using_breakdowns_as_well)
+        
         self.undo_button.clicked.connect(self.undo_wrapper)
-        self.n_keyframes_slider.valueChanged.connect(self.handle_slider_moved)
+        self.n_extreme_keyframes_slider.valueChanged.connect(self.handle_extreme_slider_moved)
+        self.n_breakdown_keyframes_slider.valueChanged.connect(self.handle_breakdown_slider_moved)
         self.close_button.clicked.connect(self.close)
     
     def open_choose_for_selection_dialog(self):
@@ -111,51 +119,91 @@ class SalientPosesGUI(StandardMayaWindow):
         end = Timeline.get_end()
         return end - start + 1
          
-    def select(self):
+    def select(self, error_type, fixed_keyframes):
         attr_indices = self.select_attr_gui.read_values_as_indices()
         if len(attr_indices) == 0:
             Report.error("You must choose at least one attributes for selection")
             return
+
+        start = Timeline.get_start()
+        end = Timeline.get_end()
+
+        attr_indices = self.select_attr_gui.read_values_as_indices()
         
-        History.start_undo_block()
+        data = []
+        f = start
+        while f <= end:
+            data.append(f)
+            for ai in attr_indices:
+                data.append(ai.read_at_time(f))
+            f += 1.0
+
+        n_frames = end - start + 1
+        max_keyframes = int(n_frames * 0.2) #int(self.max_keyframes_edit.text())
+        result = maya.cmds.salientSelect(error_type, start, end, max_keyframes, fixed_keyframes, data)
         
+        selections = {}
+        for line in result.split("\n"):
+            if line != "":
+                errorString, selectionString = line.split("|")
+                error = float(errorString)
+                selection = [int(v) for v in selectionString.split(",")]
+                n_keyframes = len(selection)
+                selections[n_keyframes] = { "selection" : selection, "error" : error }
+        
+        return selections
+        
+    def select_extremes(self):
+        fixed_keyframes = []
         try:
+            fixed_keyframes_str = self.fixed_keyframes_edit.text()
+            fixed_keyframes = [int(v) for v in fixed_keyframes_str.split(",")]
+        except ValueError:
+            pass
+        self.extreme_selections = self.select("line", fixed_keyframes)
+        self.n_extreme_keyframes_slider.setMinimum(min(self.extreme_selections.keys()))
+        self.n_extreme_keyframes_slider.setMaximum(max(self.extreme_selections.keys()))
+        self.n_extreme_keyframes_edit.setText(str(int(self.n_extreme_keyframes_slider.value())))
         
-            start = Timeline.get_start()
-            end = Timeline.get_end()
+    def select_breakdowns(self):
+        n = int(self.n_extreme_keyframes_slider.value())
+        fixed_keyframes = self.extreme_selections[n]["selection"]
+        self.breakdown_selections = self.select("curve", fixed_keyframes)
+        self.n_breakdown_keyframes_slider.setMinimum(min(self.breakdown_selections.keys()))
+        self.n_breakdown_keyframes_slider.setMaximum(max(self.breakdown_selections.keys()))
+        self.n_breakdown_keyframes_edit.setText(str(int(self.n_breakdown_keyframes_slider.value())))
 
-            attr_indices = self.select_attr_gui.read_values_as_indices()
+    def handle_extreme_slider_moved(self):
+        if len(self.extreme_selections.keys()) == 0:
+            Report.error("Please run the selection before using this slider")
+            return
             
-            data = []
-            f = start
-            while f <= end:
-                data.append(f)
-                for ai in attr_indices:
-                    data.append(ai.read_at_time(f))
-                f += 1.0
-
-            max_keyframes = int(self.max_keyframes_edit.text())
-            result = maya.cmds.salientSelect(start, end, max_keyframes, data)
-            
-            self.selections = {}
-            for line in result.split("\n"):
-                if line != "":
-                    errorString, selectionString = line.split("|")
-                    error = float(errorString)
-                    selection = [int(v) for v in selectionString.split(",")]
-                    n_keyframes = len(selection)
-                    self.selections[n_keyframes] = { "selection" : selection, "error" : error }
-              
-            self.n_keyframes_slider.setMinimum(min(self.selections.keys()))
-            self.n_keyframes_slider.setMaximum(max(self.selections.keys()))
-            self.n_keyframes_edit.setText(str(int(self.n_keyframes_slider.value())))
-            
-        except RuntimeError as e:
-            Report.error("Failed select: " + str(e))
+        n = int(self.n_extreme_keyframes_slider.value())
+        self.n_extreme_keyframes_edit.setText(str(n))
         
-        History.finish_undo_block()
+        if n not in self.extreme_selections.keys():
+            Report.error("No selection of %d keyframes was found?" % n)
+            return
+            
+        selection = self.extreme_selections[n]["selection"]
+        Visualize.ghost_keyframes_of_select(selection)
         
-    def reduce(self):
+    def handle_breakdown_slider_moved(self):
+        if len(self.breakdown_selections.keys()) == 0:
+            Report.error("Please run the selection before using this slider")
+            return
+            
+        n = int(self.n_breakdown_keyframes_slider.value())
+        self.n_breakdown_keyframes_edit.setText(str(n))
+        
+        if n not in self.breakdown_selections.keys():
+            Report.error("No selection of %d keyframes was found?" % n)
+            return
+            
+        selection = self.breakdown_selections[n]["selection"]
+        Visualize.ghost_keyframes_of_select(selection)
+        
+    def reduce(self, keyframes):
         attr_indices = self.reduce_attr_gui.read_values_as_indices()
         if len(attr_indices) == 0:
             Report.error("You must choose at least one attributes for reduction")
@@ -166,12 +214,6 @@ class SalientPosesGUI(StandardMayaWindow):
             Report.error("No keyframes found on the selected attributes?")
             return
         
-        n = int(self.n_keyframes_slider.value())
-        if n not in self.selections.keys():
-            Report.error("No selection of `%d` keyframes found for reduction?" % n)
-            return
-            
-        keyframes = [v + Timeline.get_start() for v in self.selections[n]["selection"]]
         n_keyframes = len(keyframes)
         start = keyframes[0]
         end = keyframes[-1]
@@ -181,7 +223,15 @@ class SalientPosesGUI(StandardMayaWindow):
         try:
                                 
             for ai in attr_indices:
-                result = maya.cmds.salientReduce(ai.obj, ai.attr, keyframes)
+                
+                data = []
+                f = start
+                while f <= end:
+                    data.append(f)
+                    data.append(maya.cmds.getAttr(ai.key, time=f))
+                    f += 1.0
+                    
+                result = maya.cmds.salientReduce(ai.obj, ai.attr, keyframes, data)
                 if len(result) % 8 != 0:
                     Report.error("Invalid result given by reduction command for %s?" % AttributeIndex(ai.obj, ai.attr))
                     return 
@@ -232,23 +282,18 @@ class SalientPosesGUI(StandardMayaWindow):
             
         History.finish_undo_block()
         
+    def reduce_using_extremes_only(self):
+        n = int(self.n_extreme_keyframes_slider.value())
+        keyframes = [v + Timeline.get_start() for v in self.extreme_selections[n]["selection"]]
+        self.reduce(keyframes)
+        
+    def reduce_using_breakdowns_as_well(self):
+        n = int(self.n_breakdown_keyframes_slider.value())
+        keyframes = [v + Timeline.get_start() for v in self.breakdown_selections[n]["selection"]]
+        self.reduce(keyframes)
+        
     def undo_wrapper(self):
         History.undo()
-
-    def handle_slider_moved(self):
-        if len(self.selections.keys()) == 0:
-            Report.error("Please run the selection before using this slider")
-            return
-            
-        n = int(self.n_keyframes_slider.value())
-        self.n_keyframes_edit.setText(str(n))
-        
-        if n not in self.selections.keys():
-            Report.error("No selection of %d keyframes was found?" % n)
-            return
-            
-        selection = self.selections[n]["selection"]
-        Visualize.ghost_keyframes_of_select(selection)
         
         
 if __name__ == "__main__":    
